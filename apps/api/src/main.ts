43dc95a5101e84d5a7465337c28661f5127b5aa2
Anns Shahbaz <annsshahbaz@gmail.com>
2025-10-19 15:31:18 +0500

    ...cors...

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    // origin: process.env.CORS_ORIGINS?.split(',') || [],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    maxAge: 3600,
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
