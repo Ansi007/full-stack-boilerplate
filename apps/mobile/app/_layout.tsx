53fd511143a24719ddbcdf1a40673967972736c5
Anns Shahbaz <annsshahbaz@gmail.com>
2025-10-22 14:15:19 +0500

    remove issue of ip setup on mobile

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Platform } from "react-native";
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -11,7 +12,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <TrpcProvider url={process.env.EXPO_PUBLIC_TRPC_URL!}>
+      <TrpcProvider
+        url={
+          Platform.OS === "android"
+            ? process.env.EXPO_PUBLIC_TRPC_URL_ANDROID!
+            : process.env.EXPO_PUBLIC_TRPC_URL!
+        }
+      >
         <Stack
           screenOptions={{
             headerShown: false,
