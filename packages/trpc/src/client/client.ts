d7d6a9f5cf3dbb5f32a26f87eeabfdb26a76fc9c
Anns Shahbaz <annsshahbaz@gmail.com>
2025-10-23 11:24:50 +0500

    misc.

--- packages/trpc/src/client/client.ts
+++ packages/trpc/src/client/client.ts
@@ -3,7 +3,7 @@ import {
   CreateTRPCReact,
   httpBatchLink,
 } from "@trpc/react-query";
-import { AppRouter } from "@repo/trpc/router";
+import { AppRouter } from "../server/server";
 import { QueryClient } from "@tanstack/react-query";
 
 export const trpc: CreateTRPCReact<AppRouter, object> = createTRPCReact<
