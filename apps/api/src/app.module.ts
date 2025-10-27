89fae3dafc08ca0c0b61e78e8545f90be2a638e5
Anns Shahbaz <annsshahbaz@gmail.com>
2025-10-27 17:05:06 +0500

    -- Req/Res Context --

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,17 +3,19 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 import { TRPCModule } from 'nestjs-trpc';
 import { CrudModule } from './crud/crud.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppContext } from './app.context';
 
 @Module({
   imports: [
     TRPCModule.forRoot({
       autoSchemaFile: '../../packages/trpc/src/server',
+      context: AppContext,
     }),
     CrudModule,
     PrismaModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [AppContext],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
